276ff120101490e41ee37ea98440f78d06d0eb40
Geraldo Moura <geraldo.moura@ymail.com>
2022-01-30 11:43:50 -0700

    created product repo with const to add, update, find, delete products

--- MyShop.DataAccess.InMemory/ProductRepository.cs
+++ MyShop.DataAccess.InMemory/ProductRepository.cs
@@ -11,6 +11,71 @@ namespace MyShop.DataAccess.InMemory
     public class ProductRepository
     {
         ObjectCache cachec = MemoryCache.Default;
-        List<Product> products = new List<Product>();
+        List<Product> products;
+
+        public ProductRepository()
+        {
+            products = cachec["products"] as List<Product>;
+            if(products == null)
+            {
+                products = new List<Product>();
+            }
+        }
+        public void Commit()
+        {
+            cachec["products"] = products;
+        }
+
+        public void Insert(Product p)
+        {
+            products.Add(p);
+        }
+
+        public void Update(Product product)
+        {
+            Product productToUpdate = products.Find(p => p.Id == product.Id);
+
+            if(productToUpdate != null)
+            {
+                productToUpdate = product;
+            } 
+            else
+            {
+                throw new Exception("Product not found!");
+            }
+        }
+
+        public Product Find(string Id)
+        {
+            Product product = products.Find(p => p.Id == Id);
+
+            if (product != null)
+            {
+                return product;
+            }
+            else
+            {
+                throw new Exception("Product not found.");
+            }
+        }
+
+        public IQueryable<Product> Collection()
+        {
+            return products.AsQueryable();
+        }
+
+        public void Delete(string Id)
+        {
+            Product productToDelete = products.Find(p => p.Id == Id);
+
+            if (productToDelete != null)
+            {
+                products.Remove(productToDelete);
+            }
+            else
+            {
+                throw new Exception("Product not found!");
+            }
+        } 
     }
 }
