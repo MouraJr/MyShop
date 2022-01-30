30fcdb7025933f9517d4c12577a6496fca2365f9
Geraldo Moura <geraldo.moura@ymail.com>
2022-01-30 16:46:25 -0700

    using DRY principal, created a BaseEntity to support products and categories.

--- MyShop.Core/Models/Product.cs
+++ MyShop.Core/Models/Product.cs
@@ -8,10 +8,8 @@ using System.Threading.Tasks;
 
 namespace MyShop.Core.Models
 {
-    public class Product
+    public class Product : BaseEntity
     {
-        public string Id { get; set; }
-
         [StringLength(20)]
         [DisplayName("Product Name")]
         public string Name { get; set; }
@@ -20,12 +18,6 @@ namespace MyShop.Core.Models
         [Range(0.00, 1000.00)]
         public string Price { get; set; }
         public string Category { get; set; }
-        public string Image { get; set; }
-
-        public Product()
-        {
-            this.Id = Guid.NewGuid().ToString();
-        }
-        
+        public string Image { get; set; }        
     }
 }
